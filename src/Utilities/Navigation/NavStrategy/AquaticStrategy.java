0aaf3a2c4c44458babb0920c584e2480343f9a22
Kyle Brost <kjbrost@gmail.com>
2016-04-05 16:35:09 -0400

    initial commit.  Class to request to move an aquatic entity to a tile.

--- src/Utilities/Navigation/NavStrategy/AquaticStrategy.java
+++ src/Utilities/Navigation/NavStrategy/AquaticStrategy.java
@@ -3,5 +3,9 @@ package Utilities.Navigation.NavStrategy;
 /**
  * Created by broskj on 4/5/16.
  */
-public class AquaticStrategy {
+public class AquaticStrategy implements NavStrategy {
+    @Override
+    public void move(Tile targetTile, Entity entity) {
+        //targetTile.moveAquatic(entity);
+    }
 }
