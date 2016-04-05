fe80a2e1d9c3202f1af974e1db31cb0ac8699569
Kyle Brost <kjbrost@gmail.com>
2016-04-05 16:35:50 -0400

    initial commit.  Class to request to move a boat entity to a tile.

--- src/Utilities/Navigation/NavStrategy/BoatStrategy.java
+++ src/Utilities/Navigation/NavStrategy/BoatStrategy.java
@@ -3,5 +3,9 @@ package Utilities.Navigation.NavStrategy;
 /**
  * Created by broskj on 4/5/16.
  */
-public class BoatStrategy {
+public class BoatStrategy implements NavStrategy {
+    @Override
+    public void move(Tile targetTile, Entity entity) {
+        //targetTile.moveBoat(entity);
+    }
 }
