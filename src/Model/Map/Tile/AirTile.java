79cba21d3e4ad7b26c5f24f311b81b7c8f515184
Samuel Lichtenheld <slichtenheld@ufl.edu>
2016-04-05 16:36:26 -0400

    filled out Tile classes, a lot of it is still commented out because they need the TileStrategy

---
+++ src/Model/Map/Tile/AirTile.java
@@ -0,0 +1,14 @@
+package Model.Map.Tile;
+
+/**
+ * Created by sgl on 4/5/16.
+ */
+
+//contains AirTileStrategy --> birds can fly here
+public class AirTile extends MobileTile {
+    //private AirTileStrategy airTileStrategy;
+
+    public AirTile(){
+        //super(new AirTileStrategy());
+    }
+}
---
+++ src/Model/Map/Tile/RiverTile.java
@@ -0,0 +1,18 @@
+package Model.Map.Tile;
+
+/**
+ * Created by sgl on 4/5/16.
+ */
+public class RiverTile extends WaterTile {
+    private int flowRate;
+    private int direction;
+    //private RiverTileStrategy RiverTileStrategy;
+
+    public RiverTile(){
+        //super.(new RiverTileStrategy());
+    }
+
+    private void movePlayer(){
+        //entity.move(direction);
+    }
+}
