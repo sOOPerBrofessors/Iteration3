95aab58d6aebebfbc1d60aef310f02ede859d7e0
Kyle Brost <kjbrost@gmail.com>
2016-04-06 17:45:21 -0400

    added default error printing

--- src/Model/Map/Tile/Tile.java
+++ src/Model/Map/Tile/Tile.java
@@ -1,6 +1,7 @@
 package Model.Map.Tile;
 
 import Model.Map.Tile.TileStrategy.TileStrategy;
+import Utilities.MessageHandler;
 
 /**
  * Created by sgl on 4/5/16.
@@ -14,27 +15,27 @@ public abstract class Tile {
     }
 
     public void moveAvian(){
-
+        MessageHandler.printError("You cannot move to this tile.");
     }
 
     public void moveAquatic(){
-
+        MessageHandler.printError("You cannot move to this tile.");
     }
 
     public  void moveBoat(){
-
+        MessageHandler.printError("You cannot move to this tile.");
     }
 
     public void moveChar(){
-
+        MessageHandler.printError("You cannot move to this tile.");
     }
 
     public void moveProjectile(){
-
+        MessageHandler.printError("You cannot move to this tile.");
     }
 
     public void moveVehicle(){
-
+        MessageHandler.printError("You cannot move to this tile.");
     }
 
 
