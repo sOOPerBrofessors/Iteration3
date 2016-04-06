92c286da9a39475a64b39ebf15c8acb76ad76112
Samuel Lichtenheld <slichtenheld@ufl.edu>
2016-04-06 11:46:31 -0400

    added empty TileStrategy classes for tiles so that I could uncomment constructors etc

--- src/Model/Map/Tile/Tile.java
+++ src/Model/Map/Tile/Tile.java
@@ -1,15 +1,17 @@
 package Model.Map.Tile;
 
+import Model.Map.Tile.TileStrategy.TileStrategy;
+
 /**
  * Created by sgl on 4/5/16.
  */
 public abstract class Tile {
 
-    //private TileStrategy tileStragey;
+    private TileStrategy tileStrategy;
 
-//    public Tile(TileStrategy tileStrategy){
-//        this.tileStrategy = tileStrategy;
-//    }
+    public Tile(TileStrategy tileStrategy){
+        this.tileStrategy = tileStrategy;
+    }
 
     public void moveAvian(){
 
