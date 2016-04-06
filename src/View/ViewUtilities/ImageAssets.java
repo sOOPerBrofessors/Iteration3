bbc6efb2d2214583b6a41fc67a25daf9385b64ed
Samuel Lichtenheld <slichtenheld@ufl.edu>
2016-04-06 11:26:19 -0400

    added in SpriteLoader and ImageAssets so now images should be statically accessible

--- src/View/ViewUtilities/ImageAssets.java
+++ src/View/ViewUtilities/ImageAssets.java
@@ -1,7 +1,42 @@
 package View.ViewUtilities;
 
+import java.awt.image.BufferedImage;
+
 /**
  * Created by sgl on 4/6/16.
  */
 public class ImageAssets {
+
+    private static final int width = 96; //width and height of sprites
+    private static final int height = 96;
+
+    public static BufferedImage smasherSW, smasherNW, smasherN, smasherNE, smasherSE, smasherS;
+    public static BufferedImage sneakSW, sneakNW, sneakN, sneakNE, sneakSE, sneakS;
+    public static BufferedImage grass;
+
+    //needs to be called once
+    public static void init(){
+        SpriteSheet smasherSheet = new SpriteSheet(SpriteLoader.loadImage("/textures/Smasher.png"));
+        SpriteSheet sneakSheet = new SpriteSheet(SpriteLoader.loadImage("/textures/sneak.png"));
+        SpriteSheet terrainSheet = new SpriteSheet(SpriteLoader.loadImage("/textures/terrains.png"));
+
+        smasherSW = smasherSheet.cropSheet(0,0, width, height);
+        smasherNW = smasherSheet.cropSheet(width*1, 0, width, height);
+        smasherN = smasherSheet.cropSheet(width*2, 0, width, height);
+        smasherNE = smasherSheet.cropSheet(width*3, 0, width, height);
+        smasherSE = smasherSheet.cropSheet(width*4, 0, width, height);
+        smasherS = smasherSheet.cropSheet(width*5, 0, width, height);
+
+        sneakSW = sneakSheet.cropSheet(0,0, width, height);
+        sneakNW = sneakSheet.cropSheet(width*1, 0, width, height);
+        sneakN = sneakSheet.cropSheet(width*2, 0, width, height);
+        sneakNE = sneakSheet.cropSheet(width*3, 0, width, height);
+        sneakSE = sneakSheet.cropSheet(width*4, 0, width, height);
+        sneakS = sneakSheet.cropSheet(width*5, 0, width, height);
+
+        grass = terrainSheet.cropSheet(0,0, width, height);
+
+
+
+    }
 }
--- src/View/ViewUtilities/SpriteLoader.java
+++ src/View/ViewUtilities/SpriteLoader.java
@@ -1,7 +1,18 @@
 package View.ViewUtilities;
 
-/**
- * Created by sgl on 4/6/16.
- */
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+//static class used to load in sprites, should only be used in ImageAssets class
 public class SpriteLoader {
+    public static BufferedImage loadImage(String path){
+        try {
+            return ImageIO.read(new File("res" + path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
