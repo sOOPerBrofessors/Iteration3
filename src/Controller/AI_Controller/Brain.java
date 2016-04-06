1dbcdd3868fe7e0b3cd3a17b8de5f27f0dbcd013
aseber <aseber@techsouth.cc>
2016-04-06 16:28:56 -0400

    Fixed some package issues, pushing to main next

--- src/Controller/AI_Controller/Brain.java
+++ src/Controller/AI_Controller/Brain.java
@@ -1,18 +1,18 @@
-package com.wecanteven.Controllers.AIControllers;
+package Controller.AI_Controller;
 
-import com.wecanteven.Models.ModelTime.Tickable;
+import Utilities.Tickable;
 
 /**
  * Created by aseber on 4/6/16.
  */
 public class Brain implements Tickable {
 
-    private Memory memory;
-    private VisualCortex visualCortex;
-    private FrontalLobe frontalLobe;
-    private MotorCortex motorCortex
+    //private Memory memory;
+    //private VisualCortex visualCortex;
+    //private FrontalLobe frontalLobe;
+    //private MotorCortex motorCortex
 
-    void tick() {
+    public void tick() {
 
 
 
--- src/Utilities/UniformPicker.java
+++ src/Utilities/UniformPicker.java
@@ -1,6 +1,5 @@
-package com.wecanteven.UtilityClasses;
+package Utilities;
 
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Random;
 
