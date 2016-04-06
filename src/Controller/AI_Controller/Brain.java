7f25d7e65a2ad481fa2988830efd4626ee589b52
aseber <aseber@techsouth.cc>
2016-04-06 16:26:12 -0400

    Initial commit to AI_Core, added UniformPicker Generic class, and began work on Brain

---
+++ src/Controller/AI_Controller/Brain.java
@@ -0,0 +1,21 @@
+package com.wecanteven.Controllers.AIControllers;
+
+import com.wecanteven.Models.ModelTime.Tickable;
+
+/**
+ * Created by aseber on 4/6/16.
+ */
+public class Brain implements Tickable {
+
+    private Memory memory;
+    private VisualCortex visualCortex;
+    private FrontalLobe frontalLobe;
+    private MotorCortex motorCortex
+
+    void tick() {
+
+
+
+    }
+
+}
