942b734e19435adeeb26f036883a2111b28a7fc6
Kyle Brost <kjbrost@gmail.com>
2016-04-07 19:16:31 -0400

    made Item extend from Model

--- src/Model/Items/Item.java
+++ src/Model/Items/Item.java
@@ -1,11 +1,13 @@
 package Model.Items;
 
+import Model.Model;
+
 /**
  * Created by broskj on 4/6/16.
  *
  * Abstract class used to define all things which can be put on the map and used by a character in one form or another.
  *  May be extended to be picked up by a player, may act as an obstacle, or may be interacted with by the player.
  */
-public abstract class Item {
+public abstract class Item extends Model {
 
 } // end abstract class Item
