95ec4ec844ab95c3d94f32dd48dc8d6604b9880c
Michael <mikewim@ufl.edu>
2016-04-08 05:55:10 -0400

    whatevs

--- src/Utilities/GameLoader.java
+++ src/Utilities/GameLoader.java
@@ -46,7 +46,6 @@ public class GameLoader {
                         tile = new AirTile();
                     }
                     tC.addTiles(tile);
-
                 }
             }
         }
