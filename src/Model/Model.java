3399cce3e251858ecbc68aa8de0242aab174be97
Samuel Lichtenheld <slichtenheld@ufl.edu>
2016-04-08 20:48:06 -0400

    added some tick functions

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -11,20 +11,52 @@ public class Model implements Runnable{
 
     private StateManager stateManager;
 
+    private final int TICK_RATE = 60 ;
+    private final int TARGET_TIME = 1000 / TICK_RATE;
+
     public Model(){
         stateManager = new StateManager();
     }
 
-    public void start(){
-
+    public synchronized void start(){
+        new Thread(this).start();
     }
 
+
+    //feel free to overdo this Mike, just thought I'd put it in here for now
     @Override
     public void run() {
 
+        long lastTime;
+        long delta;
+        long waitTime;
+
+        while (true) {
+
+            lastTime = System.nanoTime();
+
+            stateManager.tick();
+
+            delta = System.nanoTime() - lastTime;
+
+            waitTime = TARGET_TIME - delta / 1000000;
+
+            if (waitTime < TARGET_TIME) {
+                waitTime = TARGET_TIME;
+            }
+            try {
+                Thread.sleep(waitTime);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
+
 
     public StateManager getStateManager() {
         return stateManager;
     }
+
 }
+
--- src/Model/State/GameState/ActiveGameState.java
+++ src/Model/State/GameState/ActiveGameState.java
@@ -1,19 +1,26 @@
 package Model.State.GameState;
 
+import Controller.AI_Controller.Brain;
 import Controller.Controllers.GamePlayController;
 import Model.Entity.Character.Avatar;
 import Model.Map.Map;
 import Model.State.GameState.GameState;
+import com.sun.xml.internal.bind.v2.TODO;
 
 /**
  * Created by Wimberley on 4/6/16.
  */
+
 public class ActiveGameState extends GameState {
 
     private GamePlayController activeGameController;
+    private Brain brain;
 
     public ActiveGameState(Map map, Avatar avatar){
         super(map,avatar);
+
+        //Instantiate Brain
+        //brain = new Brain()
     }
 
     //The ActiveGameState is never creating any models. It is being created within the GameLoader and then passed in through
@@ -25,7 +32,10 @@ public class ActiveGameState extends GameState {
 
     @Override
     public void tick(){
-
+        // TODO: Brain needs to be instantiated with
+        //brain.tick(); //ticks AI
+        //possibly need to tick human input if can be inputted too fast
+        //need to tick rivers???
     }
 
     // player commands from controller
