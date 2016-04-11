6fbf87ac70fa764b8439aac72f693bfa4f9b615c
aseber <aseber@techsouth.cc>
2016-04-11 17:07:51 -0400

    Added another message to the MessageHandler paradigm

--- src/Model/Map/Tile/Tile.java
+++ src/Model/Map/Tile/Tile.java
@@ -20,32 +20,32 @@ public abstract class Tile implements TileVisitable, TileObservable{
     }
 
     public boolean moveAvian(Entity entity){
-        MessageHandler.println("You cannot move to this tile.", ErrorLevel.ERROR);
+        MessageHandler.println("You cannot move to this tile.", ErrorLevel.NOTICE);
         return false;
     }
 
     public boolean moveAquatic(Entity entity){
-        MessageHandler.println("You cannot move to this tile.", ErrorLevel.ERROR);
+        MessageHandler.println("You cannot move to this tile.", ErrorLevel.NOTICE);
         return false;
     }
 
     public boolean moveBoat(Entity entity){
-        MessageHandler.println("You cannot move to this tile.", ErrorLevel.ERROR);
+        MessageHandler.println("You cannot move to this tile.", ErrorLevel.NOTICE);
         return false;
     }
 
     public boolean moveChar(Entity entity){
-        MessageHandler.println("You cannot move to this tile.", ErrorLevel.ERROR);
+        MessageHandler.println("You cannot move to this tile.", ErrorLevel.NOTICE);
         return false;
     }
 
     public boolean moveProjectile(Entity entity){
-        MessageHandler.println("You cannot move to this tile.", ErrorLevel.ERROR);
+        MessageHandler.println("You cannot move to this tile.", ErrorLevel.NOTICE);
         return false;
     }
 
     public boolean moveVehicle(Entity entity){
-        MessageHandler.println("You cannot move to this tile.", ErrorLevel.ERROR);
+        MessageHandler.println("You cannot move to this tile.", ErrorLevel.NOTICE);
         return false;
     }
 
