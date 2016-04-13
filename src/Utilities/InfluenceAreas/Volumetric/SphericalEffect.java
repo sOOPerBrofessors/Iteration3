53c95b60b47d201493875a890fd1e3e1171fe9ed
AndyZhu <zhuyidiwow@gmail.com>
2016-04-13 18:03:31 -0400

    Set up influence areas

--- src/Utilities/InfluenceAreas/Volumetric/SphericalEffect.java
+++ src/Utilities/InfluenceAreas/Volumetric/SphericalEffect.java
@@ -1,7 +1,16 @@
 package Utilities.InfluenceAreas.Volumetric;
 
+import Model.Map.Location;
+
+import java.util.ArrayList;
+
 /**
  * Created by AndyZhu on 13/4/2016.
  */
 public class SphericalEffect {
+
+    public static ArrayList<Location> getInfluenceArea() {
+        ArrayList<Location> list = new ArrayList<>();
+        return list;
+    }
 }
