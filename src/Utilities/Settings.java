d53a477ef20487c36aa3c02fbc1803014668eb45
Michael <mikewim@ufl.edu>
2016-04-14 16:58:09 -0400

    fixed bug

--- src/Utilities/Settings.java
+++ src/Utilities/Settings.java
@@ -57,8 +57,8 @@ public class Settings {
     public static int ENTITYWIDTH = 42;
     public static int ENTITYHEIGHT = 75;
 
-    public static int POTIONWIDTH = 38;
-    public static int POTIONHEIGHT = 42;
+    public static int POTIONWIDTH = 32;
+    public static int POTIONHEIGHT = 32;
     //Grass flat
 //    public static int TILEWIDTH = 96;
 //    public static int TILEHEIGHT = 62;
