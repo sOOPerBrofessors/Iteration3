05224d428bd66439edfa5eddf41025fbcf471909
AndyZhu <zhuyidiwow@gmail.com>
2016-04-13 22:33:52 -0400

    Fix minor bugs when merging

--- src/Utilities/InfluenceAreas/Linear/LinearEffect.java
+++ src/Utilities/InfluenceAreas/Linear/LinearEffect.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 /**
  * Created by AndyZhu on 13/4/2016.
  */
-public class LinearEffect implements InfluenceArea{
+public class LinearEffect extends InfluenceArea{
 
     public static ArrayList<Location> getInfluencedAreaTop(int length) {
         ArrayList<Location> list = new ArrayList<>();
