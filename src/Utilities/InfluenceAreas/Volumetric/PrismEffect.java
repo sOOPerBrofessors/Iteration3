5b523f032e984caf36ebc8a7f1293fc520deb404
AndyZhu <zhuyidiwow@gmail.com>
2016-04-14 19:21:19 -0400

    PrismEffect and ConicalEffect done
    also modified linearEffect to include the origin location
    added comments to angular

--- src/Utilities/InfluenceAreas/Volumetric/PrismEffect.java
+++ src/Utilities/InfluenceAreas/Volumetric/PrismEffect.java
@@ -8,10 +8,17 @@ import java.util.ArrayList;
 
 /**
  * Created by AndyZhu on 13/4/2016.
+ *
+ * This class has four public methods
+ * 1st get all locations in the prism with full height
+ * 2nd get all locations in the prism with full height, with radius
+ * 3rd get all locations in the prism with height higher than the location passed in
+ * 4th get all locations in the prism with height higher than the location passed in, with radius
  */
 public class PrismEffect extends InfluenceArea {
 
-    /* Example of use
+    /** Example of use */
+    /*
     ArrayList<Location> list = PrismEffect.getPrismVolumeFullHeight(new Location(11,3,1),2);
     for (Location aList : list) {
         System.out.println("(" + aList.getX() + ", " + aList.getY() + ", " + aList.getZ() + ")");
@@ -28,14 +35,15 @@ public class PrismEffect extends InfluenceArea {
         ArrayList<Location> planerAreaList = RadialEffect.getRadialArea(location, radius);
 
         for (Location curLocation : planerAreaList) {
-            list = addLocationsInSameColumn(list, curLocation);
+            list = addLocationsInSameColumnFullHeight(list, curLocation);
         }
 
         list = removeOutOfMapLocations(list);
         return list;
     }
 
-    /* Example of use
+    /** Example of use */
+    /*
     ArrayList<ArrayList<Location>> list = PrismEffect.getPrismVolumeFullHeightWithRadius(new Location(11,3,1),2);
 
     for (int i = 0; i < list.size(); i++) {
@@ -55,18 +63,56 @@ public class PrismEffect extends InfluenceArea {
         ArrayList<Location> innerList;
         ArrayList<ArrayList<Location>> planerAreaList = RadialEffect.getAreaWithRadius(location, radius);
 
+        //traverse innerLists, i.e. circles
         for (ArrayList<Location> aPlanerAreaList : planerAreaList) {
             innerList = new ArrayList<>();
+            //do stuff with the locations of the same circle
             for (Location curLocation : aPlanerAreaList) {
-                innerList = addLocationsInSameColumn(innerList, curLocation);
+                innerList = addLocationsInSameColumnFullHeight(innerList, curLocation);
             }
+
             innerList = removeOutOfMapLocations(innerList);
             list.add(innerList);
         }
         return list;
     }
 
-    private static ArrayList<Location> addLocationsInSameColumn(ArrayList<Location> list, Location location) {
+    /**
+     * Use this method the same way with getPrismVolumeFullHeight()
+     * @param location : Location
+     * @param radius : int
+     * @return an ArrayList with all tiles in the prism volume
+     */
+    public static ArrayList<Location> getPrismVolumeHigherThanLocationPassedIn(Location location, int radius) {
+        int height = location.getZ();
+        ArrayList<Location> list = getPrismVolumeFullHeight(location, radius);
+        list = removeLocationsLowerThan(list, height);
+        return list;
+    }
+
+    /**
+     * Use this method the same way with getPrismVolumeFullHeightWithRadius()
+     * @param location : Location
+     * @param radius : int
+     * @return an ArrayList of ArrayList of Locations with all tiles in the prism volume with same radius,
+     * and at the same time higher or on the same level, with respect to the height of location passed in
+     */
+    public static ArrayList<ArrayList<Location>> getPrismVolumeHigherThanLocationPassedInWithRadius (Location location, int radius) {
+        int height = location.getZ();
+        ArrayList<ArrayList<Location>> list = getPrismVolumeFullHeightWithRadius(location, radius);
+        ArrayList<ArrayList<Location>> validatedList = new ArrayList<>();
+        for (ArrayList<Location> aList : list) {
+            aList = removeLocationsLowerThan(aList, height);
+            validatedList.add(aList);
+        }
+        return validatedList;
+    }
+
+
+    /**
+     * Below are private methods
+     */
+    private static ArrayList<Location> addLocationsInSameColumnFullHeight(ArrayList<Location> list, Location location) {
         Location temp = new Location(location.getX(), location.getY(), 0);
         int zLimit = 10; //largest index of vertical tiles
         for (int z = 0; z <= zLimit; z++) {
@@ -74,5 +120,14 @@ public class PrismEffect extends InfluenceArea {
             temp = temp.getAdjacentTop();
         }
         return list;
+    }
+
+    private static ArrayList<Location> removeLocationsLowerThan(ArrayList<Location> list, int height) {
+        ArrayList<Location> newList = new ArrayList<>();
+        for (Location aList : list) {
+            if (aList.getZ() >= height)
+                newList.add(aList);
+        }
+        return newList;
     }
 }
