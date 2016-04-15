ef2ec0d52f7e32ce5b90af9e056dacadeebd5573
Kyle Brost <kjbrost@gmail.com>
2016-04-15 18:23:15 -0400

    removed debug statements

--- src/Utilities/Settings.java
+++ src/Utilities/Settings.java
@@ -12,9 +12,9 @@ public class Settings {
     public static int GAMEWIDTH = 1080;
     public static int GAMEHEIGHT = 720;
     public static int SCALEFACTOR = 1;
-    public static boolean debug = true;
-    public static ErrorLevel errorLevel = ErrorLevel.NOTICE;
-    public static PersonFilter personFilter = PersonFilter.ALL;
+    public static boolean debug = false;
+    public static ErrorLevel errorLevel = ErrorLevel.NONE;
+    public static PersonFilter personFilter = PersonFilter.UNSPECIFIED;
 
     // Key Presses
     public static int UP_ARROW = KeyEvent.VK_UP;
