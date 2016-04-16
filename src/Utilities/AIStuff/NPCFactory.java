062652180b4c20da67a6eec275536cd11c8d5c63
David <dyeung93@hotmail.com>
2016-04-16 16:38:47 -0400

    dialogue works

--- src/Utilities/AIStuff/NPCFactory.java
+++ src/Utilities/AIStuff/NPCFactory.java
@@ -5,6 +5,8 @@ import Controller.AI_Controller.Personality.Personality;
 import Controller.AI_Controller.Personality.PersonalityFactory;
 import Model.Entity.Character.Character;
 import Model.Entity.Character.NPC.NPC;
+import Model.Entity.Character.NPC.NPCStrategy.NPCStrategy;
+import Model.Entity.Character.NPC.NPCStrategy.TalkNPCStrategy;
 import Model.Entity.Character.Occupation.Smasher;
 import Model.Entity.Entity;
 import Model.Faction.Faction;
@@ -21,7 +23,9 @@ public class NPCFactory {
     public static NPC makeGanondorf(){
         Personality personality = PersonalityFactory.getPersonality("enemy");
         Faction faction = FactionFactory.getFaction("red");
-        NPC gandorf = new NPC(new Smasher(), new Location(1,1,0), personality, faction);
+        //This should be a factory, testing for now
+        NPCStrategy strategy = new TalkNPCStrategy("Hello world", "I am awesome");
+        NPC gandorf = new NPC(new Smasher(), new Location(1,1,0), personality, faction, strategy);
         Brain brain = new Brain(gandorf);
         gandorf.setBrain(brain);
         return gandorf;
