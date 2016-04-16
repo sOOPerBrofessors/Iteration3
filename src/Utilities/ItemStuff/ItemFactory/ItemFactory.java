eb16a34e19fffeb4d409a97a6a8f50c5981e9595
Kyle Brost <kjbrost@gmail.com>
2016-04-16 17:28:24 -0400

    added new factory methods to make potions, weapons, and armors of different types

--- src/Utilities/ItemStuff/ItemFactory/ItemFactory.java
+++ src/Utilities/ItemStuff/ItemFactory/ItemFactory.java
@@ -4,7 +4,7 @@ import Model.Items.Interactable.Chest;
 import Model.Items.Interactable.Interactable;
 import Model.Items.Item;
 import Model.Items.Takeable.Equippable.Armor;
-import Model.Items.Takeable.Equippable.Weapon;
+import Model.Items.Takeable.Equippable.Weapon.Weapon;
 import Model.Items.Takeable.TakeableItem;
 import Model.Items.Takeable.Useable.Potion;
 import Model.Map.Location;
@@ -47,7 +47,7 @@ public class ItemFactory {
         itemViews.put(sword, swordView);
 
         // create chest armor
-        Armor chestArmor = ItemFactory.makeLightChainmail();
+        Armor chestArmor = ItemFactory.makeLightArmor();
         ItemView chestArmorView = new UsableView(ImageAssets.chestPlate);
         Location chestArmorLocation = new Location(3,3,map.getTopTilePosition(3,3));
         chestArmorView.setLocation(3,3);
@@ -63,59 +63,198 @@ public class ItemFactory {
         itemViews.put(closedChest,closedChestView);
     }
 
+    /*
+    potion factory methods
+    three of each type
+     */
+    public static Potion makeBasicAgilityPotion() {
+        return PotionFactory.makeAgilityPotion("Basic", 3);
+    } // end factory method makeBasicAgilityPotion
+
+    public static Potion makeGoodAgilityPotion() {
+        return PotionFactory.makeAgilityPotion("Good", 6);
+    } // end factory method makeGoodAgilityPotion
+
+    public static Potion makeSuperAgilityPotion() {
+        return PotionFactory.makeAgilityPotion("Super", 10);
+    } // end factory method makeSuperAgilityPotion
+
+    public static Potion makeBasicHardinessPotion() {
+        return PotionFactory.makeHardinessPotion("Basic", 3);
+    } // end factory method makeBasicHardinessPotion
+
+    public static Potion makeGoodHardinessPotion() {
+        return PotionFactory.makeHardinessPotion("Good", 6);
+    } // end factory method makeGoodHardinessPotion
+
+    public static Potion makeSuperHardinessPotion() {
+        return PotionFactory.makeHardinessPotion("Super", 10);
+    } // end factory method makeSuperHardinessPotion
+
+    public static Potion makeBasicIntellectPotion() {
+        return PotionFactory.makeIntellectPotion("Basic", 3);
+    } // end factory method makeBasicIntellectPotion
+
+    public static Potion makeGoodIntellectPotion() {
+        return PotionFactory.makeIntellectPotion("Good", 6);
+    } // end factory method makeGoodIntellectPotion
+
+    public static Potion makeSuperIntellectPotion() {
+        return PotionFactory.makeIntellectPotion("Super", 10);
+    } // end factory method makeSuperIntellectPotion
+
     public static Potion makeBasicHealthPotion() {
-        return Potion.makeHealthPotion("Basic Health potion",
-                "A basic health potion (+3)",
-                3);
-    } // end factory method makeHealthPotion
+        return PotionFactory.makeHealthPotion("Basic", 3);
+    } // end factory method makeBasicHealthPotion
 
     public static Potion makeGoodHealthPotion() {
-        return Potion.makeHealthPotion("Good Health potion",
-                "A good health potion (+6)",
-                6);
-    } // end factory method makeHealthPotion
+        return PotionFactory.makeHealthPotion("Good", 6);
+    } // end factory method makeGoodHealthPotion
 
     public static Potion makeSuperHealthPotion() {
-        return Potion.makeHealthPotion("Super Health potion",
-                "A super health potion (+10)",
-                10);
-    } // end factory method makeHealthPotion
+        return PotionFactory.makeHealthPotion("Super", 10);
+    } // end factory method maakeSuperHealthPotion
 
+    public static Potion makeBasicLifePotion() {
+        return PotionFactory.makeLifePotion("Basic", 1);
+    } // end factory method makeBasicLifePotion
+
+    public static Potion makeGoodLifePotion() {
+        return PotionFactory.makeLifePotion("Good", 2);
+    } // end factory method makeGoodLifePotion
+
+    public static Potion makeSuperLifePotion() {
+        return PotionFactory.makeLifePotion("Super", 3);
+    } // end factory method makeSuperLifePotion
+
+    public static Potion makeBasicManaPotion() {
+        return PotionFactory.makeManaPotion("Basic", 3);
+    } // end factory method makeBasicManaPotion
+
+    public static Potion makeGoodManaPotion() {
+        return PotionFactory.makeManaPotion("Good", 6);
+    } // end factory method makeGoodManaPotion
+
+    public static Potion makeSuperManaPotion() {
+        return PotionFactory.makeManaPotion("Super", 10);
+    } // end factory method maakeSuperManaPotion
+
+    public static Potion makeBasicMovementPotion() {
+        return PotionFactory.makeMovementPotion("Basic", 3);
+    } // end factory method makeBasicMovementPotion
+
+    public static Potion makeGoodMovementPotion() {
+        return PotionFactory.makeMovementPotion("Good", 6);
+    } // end factory method makeGoodMovementPotion
+
+    public static Potion makeSuperMovementPotion() {
+        return PotionFactory.makeMovementPotion("Super", 10);
+    } // end factory method maakeSuperMovementPotion
+
+    public static Potion makeBasicStrengthPotion() {
+        return PotionFactory.makeStrengthPotion("Basic", 3);
+    } // end factory method makeBasicStrengthPotion
+
+    public static Potion makeGoodStrengthPotion() {
+        return PotionFactory.makeStrengthPotion("Good", 6);
+    } // end factory method makeGoodStrengthPotion
+
+    public static Potion makeSuperStrengthPotion() {
+        return PotionFactory.makeStrengthPotion("Super", 10);
+    } // end factory method maakeSuperStrengthPotion
+
+    /*
+    weapon factory methods
+    three of each one handed weapon for all occupations,
+      and three two handed weapons for smashers
+     */
     public static Weapon makeWoodenSword() {
-        return Weapon.makeSmasherWeapon("Wooden Sword",
-                "A wooden sword for warriors (+2)",
-                2);
-    } // end factory method makeSmasherWeapon
+        return WeaponFactory.makeOneHandedSmasherWeapon("Wooden", 3);
+    } // end factory method makeWoodenSword
 
     public static Weapon makeSteelSword() {
-        return Weapon.makeSmasherWeapon("Steel Sword",
-                "A steel sword for warriors (+5)",
-                5);
-    } // end factory method makeSmasherWeapon
+        return WeaponFactory.makeOneHandedSmasherWeapon("Steel", 5);
+    } // end factory method makeSteelSword
 
     public static Weapon makeDiamondSword() {
-        return Weapon.makeSmasherWeapon("Diamond Sword",
-                "A Diamond sword for warriors (+9)",
-                9);
-    } // end factory method makeSmasherWeapon
+        return WeaponFactory.makeOneHandedSmasherWeapon("Diamond", 9);
+    } // end factory method makeDiamondSword
 
-    public static Armor makeLightChainmail () {
-        return Armor.makeSmasherArmor("Light Chainmail",
-                "Some basic chainmail armor for warriors",
-                2);
-    } // end factory method makeSmasherWeapon
+    public static Weapon makeWoodenGreatword() {
+        return WeaponFactory.makeTwoHandedSmasherWeapon("Wooden", 4);
+    } // end factory method makeWoodenGreatsword
 
-    public static Armor makeSteelArmor () {
-        return Armor.makeSmasherArmor("Steel Armor",
-                "Some steel platemail for warriors",
-                2);
-    } // end factory method makeSmasherWeapon
+    public static Weapon makeSteelGreatsword() {
+        return WeaponFactory.makeTwoHandedSmasherWeapon("Steel", 8);
+    } // end factory method makeSteelGreatsword
 
-    public static Armor makeDiamondArmor () {
-        return Armor.makeSmasherArmor("Light Chainmail",
-                "Some diamond platemail for warriors",
-                2);
-    } // end factory method makeSmasherWeapon
+    public static Weapon makeDiamondGreatsword() {
+        return WeaponFactory.makeTwoHandedSmasherWeapon("Diamond", 16);
+    } // end factory method makeDiamondGreatsword
+
+    public static Weapon makeWoodenStaff() {
+        return WeaponFactory.makeSummonerWeapon("Wooden", 2);
+    } // end factory method makeWoodenStaff
+
+    public static Weapon makeEnchantedStaff() {
+        return WeaponFactory.makeOneHandedSmasherWeapon("Enchanted", 4);
+    } // end factory method makePotentStaff
+
+    public static Weapon makeSpectralStaff() {
+        return WeaponFactory.makeOneHandedSmasherWeapon("Spectral", 7);
+    } // end factory method makeSpectralStaff
+
+    public static Weapon makeWoodenBow() {
+        return WeaponFactory.makeSneakWeapon("Wooden", 3);
+    } // end factory method makeWoodenBow
+
+    public static Weapon makeSightedBow() {
+        return WeaponFactory.makeSneakWeapon("Sighted", 5);
+    } // end factory method makePotentStaff
+
+    public static Weapon makeCompoundBow() {
+        return WeaponFactory.makeSneakWeapon("Spectral", 9);
+    } // end factory method makeCompoundBow
+
+    /*
+    armor factory methods
+    three of each for all occupations
+     */
+    public static Armor makeLightArmor() {
+        return ArmorFactory.makeSmasherArmor("Light", 5);
+    } // end factory method makeLightArmor
+
+    public static Armor makeChainmailArmor() {
+        return ArmorFactory.makeSmasherArmor("Chainmail", 8);
+    } // end factory method makeChainmailArmor
+
+    public static Armor makePlatemailArmor() {
+        return ArmorFactory.makeSmasherArmor("Platemail", 14);
+    } // end factory method makeCPlatemailArmor
+
+    public static Armor makeTatteredRobes() {
+        return ArmorFactory.makeSummonerArmor("Tattered", 3);
+    } // end factory method makeTatteredRobes
+
+    public static Armor makeMysticalRobes() {
+        return ArmorFactory.makeSummonerArmor("Mystical", 6);
+    } // end factory method makeTatteredRobes
+
+    public static Armor makeSpellbindingRobes() {
+        return ArmorFactory.makeSummonerArmor("Spellbinding", 10);
+    } // end factory method makeTatteredRobes
+
+    public static Armor makeLeatherArmor() {
+        return ArmorFactory.makeSneakArmor("Leather", 3);
+    } // end factory method makeLeatherArmor
+
+    public static Armor makeStuddedArmor() {
+        return ArmorFactory.makeSneakArmor("Studded", 3);
+    } // end factory method makeStuddedArmor
+
+    public static Armor makeShroudedArmor() {
+        return ArmorFactory.makeSneakArmor("Shrouded", 3);
+    } // end factory method makeShroudedArmor
 
     public static HashMap<Location, TakeableItem> getTakableItems(){
         return takableItems;
--- src/Utilities/ItemStuff/ItemFactory/PotionFactory.java
+++ src/Utilities/ItemStuff/ItemFactory/PotionFactory.java
@@ -1,7 +1,76 @@
 package Utilities.ItemStuff.ItemFactory;
 
+import Model.Items.Takeable.Useable.Potion;
+
 /**
  * Created by broskj on 4/16/16.
+ *
+ * A potion factory.  Given a string for the grade of potion and and integer modification value, a different name and
+ *  description will be generated.
  */
 public class PotionFactory {
-}
+
+    public static Potion makeAgilityPotion(String grade, int amount) {
+        return Potion.makeManaPotion(
+                grade + " Agility Potion",
+                "A " + grade.toLowerCase() + " agility potion (+" + amount + ")",
+                amount
+        );
+    } // end factory method makeAgilityPotion
+
+    public static Potion makeHardinessPotion(String grade, int amount) {
+        return Potion.makeManaPotion(
+                grade + " Hardiness Potion",
+                "A " + grade.toLowerCase() + " hardiness potion (+" + amount + ")",
+                amount
+        );
+    } // end factory method makeHardinessPotion
+
+    public static Potion makeIntellectPotion(String grade, int amount) {
+        return Potion.makeManaPotion(
+                grade + " Intellect Potion",
+                "A " + grade.toLowerCase() + " intellect potion (+" + amount + ")",
+                amount
+        );
+    } // end factory method makeIntellectPotion
+
+    public static Potion makeHealthPotion(String grade, int amount) {
+        return Potion.makeHealthPotion(
+                grade + " Health Potion",
+                "A " + grade.toLowerCase() + " health potion (+" + amount +")",
+                amount
+        );
+    } // end factory method makeHealthPotion
+
+    public static Potion makeLifePotion(String grade, int amount) {
+        return Potion.makeManaPotion(
+                grade + " Life Potion",
+                "A " + grade.toLowerCase() + " Life potion (+" + amount + ")",
+                amount
+        );
+    } // end factory method makeLifePotion
+
+    public static Potion makeManaPotion(String grade, int amount) {
+        return Potion.makeManaPotion(
+                grade + " Mana Potion",
+                "A " + grade.toLowerCase() + " health potion (+" + amount + ")",
+                amount
+        );
+    } // end factory method makeManaPotion
+
+    public static Potion makeMovementPotion(String grade, int amount) {
+        return Potion.makeManaPotion(
+                grade + " Movement Potion",
+                "A " + grade.toLowerCase() + " movement potion (+" + amount + ")",
+                amount
+        );
+    } // end factory method makeMovementPotion
+
+    public static Potion makeStrengthPotion(String grade, int amount) {
+        return Potion.makeManaPotion(
+                grade + " Strength Potion",
+                "A " + grade.toLowerCase() + " strength potion (+" + amount + ")",
+                amount
+        );
+    } // end factory method makeStrengthPotion
+} // end class PotionFactory
