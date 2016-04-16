34d9f9c68f887afdfe936fd0e163981bfe733481
Kyle Brost <kjbrost@gmail.com>
2016-04-16 14:34:59 -0400

    added functionality for text-wrap for long game messages

--- src/Utilities/GameMessageQueue.java
+++ src/Utilities/GameMessageQueue.java
@@ -8,12 +8,20 @@ import java.util.ArrayList;
  * This class manages game messages on the screen.  Only five strings are displayed at a time.
  *
  * Class uses an ArrayList to simulate a FIFO queue.
+ *
+ * Any messages with length over a
  */
 public class GameMessageQueue {
     public static ArrayList<String> queue = new ArrayList<>();
     final static int cap = 5;
 
     public static void push(String message) {
+        while(message.length() > Settings.CHAR_LIMIT) {
+            int startIndex = message.length() - message.length() % Settings.CHAR_LIMIT;
+            String newString = " " + message.substring(startIndex, message.length());
+            push(newString);
+            message = message.substring(0, message.length()-newString.length()+1);
+        }
         if (queue.size() >= cap) {
             queue.remove(0);
         }
