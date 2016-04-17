797aebcea2a5e140df272d2acaff353701ba80f7
AndyZhu <zhuyidiwow@gmail.com>
2016-04-17 19:50:22 -0400

    set up

--- src/Model/Skills/SkillsWithDuration/Boon.java
+++ src/Model/Skills/SkillsWithDuration/Boon.java
@@ -1,15 +1,41 @@
 package Model.Skills.SkillsWithDuration;
 
 import Model.Entity.Character.Avatar;
-
+import Model.Map.Map;
 /**
  * Created by AndyZhu on 13/4/2016.
+ *
+ * magic that heals, temporarily grants (partial) immunities and defensive bonuses, improves stats, and other beneficial things.
  */
 public class Boon extends SkillWithDuration{
+
+    private int modifier;
+
     public Boon (Avatar avatar) {
         super (avatar);
         name = "Boon";
         manaCost = 1;
         coolDownTime = 1;
+    }
+
+
+    @Override
+    public void execute (Map map) {
+        if (allConditionChecked()) {
+            boon();
+        }
+    }
+
+    private void boon() {
+        modifier = level;
+
+    }
+
+    public void activateBoon() {
+
+    }
+
+    public void endBoon() {
+
     }
 }
